c31a2920a873d51603af803e211d1cb3a08f8c06
rubenMorales8 <ruben.morales8@upr.edu>
2025-02-26 15:06:56 -0400

    added c++ examples

---
+++ coding_basics/c++/01_c++.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string> // Needed for using the string type
+
+using namespace std;
+
+int main() {
+
+    /* EDIT ME: Type your name here 
+    string name = "YOUR NAME" */
+    string name;
+
+    /* EDIT ME: Type your age here
+    int age = YOUR AGE */
+    int age;
+    
+    cout << "Hello, " << name << "! You are " << age << " years old." << endl;
+
+    return 0;
+}
