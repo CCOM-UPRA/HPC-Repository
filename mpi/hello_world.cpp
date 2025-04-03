5504520ef96c899e98336c8a1ef8ab776a7a1d97
rubenMorales8 <ruben.morales8@upr.edu>
2025-04-03 04:45:26 -0400

    Added MPI Hello World example

--- mpi/hello_world.cpp
+++ mpi/hello_world.cpp
@@ -6,6 +6,7 @@ int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
 
     int world_rank, world_size;
+    /* Add the two missing functions of MPI*/
     /* Insert code here*/
 
     cout << "Hello world from process " << world_rank << " out of " << world_size << endl;
